8e834f89e903724b268145ead5d8005f6160c7c8
GreenyAlien1029 <112676328+GreenyAlien1029@users.noreply.github.com>
2025-08-17 15:59:26 -0700

    Add files via upload

---
+++ chem+.js
@@ -0,0 +1,124 @@
+// This is how to add a new mod to the game.
+
+// Create a new Javascript file like this one.
+// Add the file to the mods folder on GitHub, or host it somewhere else.
+// https://github.com/R74nCom/sandboxels/tree/main/mods
+
+// For more help with modding, check the wiki: https://sandboxels.wiki.gg/wiki/Modding_tutorial
+// Or join our Discord: https://r74n.com/discord/
+
+// To add it in the Mod Loader:
+// If it is in the mods folder, you can just use the name of the file. (example_mod.js)
+// If it is hosted somewhere else, you can use the full URL, including the HTTPS://.
+
+// Adding elements:
+elements.toothpaste = {
+    color: "#ffffff",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    viscosity: 1000,
+    state: "liquid",
+    density: 1500
+    breakInto: ["sodiumfluoride","calciumcarbonate","silica","glycerin","sls"]
+};
+
+elements.sodiumFlouride = {
+    color: "#ffffff",
+    behavior: behavior.POWDER,
+    category: “powders”,
+    density: 2558,
+}; 
+
+elements.calciumcarbonate = {
+    name: "Calcium Carbonate",
+    alias: ["CaCO3", "chalk"],
+    desc: "Abrasive powder, also known as chalk.",
+    color: "#e0e0e0",
+    behavior: behaviors.POWDER,
+    category: "elements",
+    state: "solid",
+    density: 2710,
+    viscosity: 0
+};
+
+elements.silica = {
+    name: "Silica",
+    alias: ["SiO2"],
+    desc: "Abrasive mineral, used in toothpaste.",
+    color: "#dddddd",
+    behavior: behaviors.POWDER,
+    category: "elements",
+    state: "solid",
+    density: 2650,
+    viscosity: 0
+};
+
+elements.glycerin = {
+    name: "Glycerin",
+    desc: "A sweet, syrupy liquid. Keeps toothpaste moist.",
+    color: "#ccccff",
+    behavior: behaviors.LIQUID,
+    category: "elements",
+    state: "liquid",
+    density: 1260,
+    viscosity: 1412 // quite thick
+};
+
+elements.sls = {
+    name: "Sodium Lauryl Sulfate",
+    alias: ["SLS"],
+    desc: "A detergent and foaming agent.",
+    color: "#fff0f0",
+    behavior: behaviors.POWDER,
+    category: "elements",
+    state: "solid",
+    density: 1000,
+    viscosity: 0
+};
+
+// Adding behavior presets:
+behaviors.SELFDELETE = [
+    "XX|XX|XX",
+    "XX|DL|XX",
+    "XX|XX|XX"
+];
+
+// Raw JavaScript behaviors:
+behaviors.mud.tick = function(pixel) {
+    if (tryMove(pixel, pixel.x, pixel.y+1)) {
+        console.log("Moved!");
+    }
+    else {
+        console.log("Couldn't move!")
+    }
+};
+
+// Create a new tool:
+elements.sand_exploder = {
+    color: "#ff0000",
+    tool: function(pixel) {
+        if (pixel.element == "sand") {
+            pixel.element = "explosion"
+        }
+    },
+    category: "tools",
+};
+
+// Run after all mods are loaded, for cross-mod compatibility
+runAfterLoad(function() {
+    // Your code here
+    console.log("Hello World!");
+});
+
+// Run if another mod is active
+dependOn("mods/chem.js", function(){
+    console.log("Hello World!");
+})
+// More dependency info: See dependency_test.js
+
+// Creating eLists:
+eLists.CONDIMENT = ["ketchup","melted_cheese","mayo"];
+// Adding elements to eLists:
+eLists.CONDIMENT.push("honey");
+
+// Rendering: See 1.10example.js
